250343d1b87e5b2b9b5ee8c19d8369445334b86e
Frederic Vogels <frederic.vogels@ucll.be>
2023-06-16 14:18:56 +0200

    refactored material tree

--- progtool/material/tree.py
+++ progtool/material/tree.py
@@ -14,6 +14,18 @@ class MaterialTreeNode(ABC):
     __path: Path
     __tree_path: TreePath
 
+    @staticmethod
+    def create(path: Path, tree_path: TreePath) -> Optional[MaterialTreeNode]:
+        if Section.test(path):
+            return Section(path, tree_path)
+        elif Exercise.test(path):
+            return Exercise(path, tree_path)
+        elif Explanation.test(path):
+            return Explanation(path, tree_path)
+        else:
+            return None
+
+
     def __init__(self, path: Path, tree_path: TreePath):
         self.__path = path
         self.__tree_path = tree_path
@@ -48,6 +60,10 @@ class MaterialTreeLeaf(MaterialTreeNode):
 
 
 class Explanation(MaterialTreeLeaf):
+    @staticmethod
+    def test(path: Path) -> bool:
+        return os.path.isfile(path / 'explanations.md')
+
     def __str__(self) -> str:
         return f'Explanation[{self.tree_path}]'
 
@@ -59,6 +75,10 @@ class Explanation(MaterialTreeLeaf):
 
 
 class Exercise(MaterialTreeLeaf):
+    @staticmethod
+    def test(path: Path) -> bool:
+        return os.path.isfile(path / 'assignment.md')
+
     def __str__(self) -> str:
         return f'Exercise[{self.tree_path}]'
 
@@ -70,28 +90,36 @@ class Exercise(MaterialTreeLeaf):
 
 
 class MaterialTreeBranch(MaterialTreeNode):
-    __children: Optional[dict[str, MaterialTreeNode]]
+    __children_table_value: Optional[dict[str, MaterialTreeNode]]
 
     def __init__(self, path: Path, tree_path: TreePath):
         super().__init__(path, tree_path)
-        self.__children = None
+        self.__children_table_value = None
 
     def __getitem__(self, key: str) -> MaterialTreeNode:
-        return self.children[key]
+        return self.__children_table[key]
 
     @property
-    def children(self) -> dict[str, MaterialTreeNode]:
-        if self.__children is None:
-            self.__children = self.__compute_children()
-        return self.__children
+    def children(self) -> list[MaterialTreeNode]:
+        return list(self.__children_table.values())
+
+    @property
+    def __children_table(self) -> dict[str, MaterialTreeNode]:
+        if self.__children_table_value is None:
+            self.__children_table_value = self.__compute_children()
+        return self.__children_table_value
 
     def __compute_children(self) -> dict[str, MaterialTreeNode]:
         entries = os.listdir(self.path)
-        nodes = (_create_node(self.path / entry, self.tree_path / entry) for entry in entries) # Can contain None values
+        nodes = (MaterialTreeNode.create(self.path / entry, self.tree_path / entry) for entry in entries) # Can contain None values
         return {node.name: node for node in nodes if node}
 
 
 class Section(MaterialTreeBranch):
+    @staticmethod
+    def test(path: Path) -> bool:
+        return os.path.isfile(path / 'section.yaml')
+
     def __str__(self) -> str:
         return f'Section[{self.tree_path}]'
 
@@ -102,35 +130,8 @@ class Section(MaterialTreeBranch):
         return 'section'
 
 
-def _create_node(path: Path, tree_path: TreePath) -> Optional[MaterialTreeNode]:
-    if _is_exercise_node(path):
-        logging.debug(f'{path} recognized as exercise')
-        return Exercise(path, tree_path)
-    elif _is_explanations_node(path):
-        logging.debug(f'{path} recognized as explanations')
-        return Explanation(path, tree_path)
-    elif _is_section_node(path):
-        logging.debug(f'{path} recognized as section')
-        return Section(path, tree_path)
-    else:
-        logging.debug(f'{path} not recognized as node')
-        return None
-
-
-def _is_exercise_node(path: Path) -> bool:
-    return os.path.isfile(path / 'assignment.md')
-
-
-def _is_explanations_node(path: Path) -> bool:
-    return os.path.isfile(path / 'explanations.md')
-
-
-def _is_section_node(path: Path) -> bool:
-    return os.path.isfile(path / 'section.yaml')
-
-
 def create_material_tree(root_path: Path) -> MaterialTreeNode:
     empty_tree_path = TreePath()
-    root = _create_node(root_path, empty_tree_path)
+    root = MaterialTreeNode.create(root_path, empty_tree_path)
     assert root is not None, 'root_path points to an empty tree' # TODO: Make exception
     return root
