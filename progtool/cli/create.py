9f559ced7c1cc578e5363527feb6699c0a56b337
Frederic Vogels <frederic.vogels@ucll.be>
2023-06-20 18:25:50 +0200

    create exercise command

--- progtool/cli/create.py
+++ progtool/cli/create.py
@@ -4,7 +4,7 @@ from progtool.cli import util
 from pathlib import Path
 import yaml
 
-from progtool.material.metadata import SectionMetadata, TYPE_SECTION
+from progtool.material.metadata import SectionMetadata, TYPE_SECTION, TYPE_EXERCISE
 
 
 @click.group()
@@ -22,3 +22,21 @@ def section(name):
     with open(dirname / 'metadata.yaml', 'w') as file:
         metadata = SectionMetadata(name=name, type=TYPE_SECTION)
         yaml.dump(metadata.dict(), file, default_flow_style=False)
+
+
+@create.command(help='Create new exercise')
+@click.argument("name", type=str)
+def exercise(name):
+    number = util.find_lowest_unused_number(util.find_numbered_subdirectories())
+    slug = util.make_slug(name)
+    dirname = Path(util.add_number(slug, number))
+    os.mkdir(dirname)
+    with open(dirname / 'metadata.yaml', 'w') as file:
+        metadata = SectionMetadata(name=name, type=TYPE_EXERCISE)
+        yaml.dump(metadata.dict(), file, default_flow_style=False)
+    with open(dirname / 'assignment.md', 'w') as file:
+        file.write(f'# {name}\n\nTODO')
+    with open(dirname / 'solution.py', 'w') as file:
+        file.write('TODO')
+    with open(dirname / 'tests.py', 'w') as file:
+        file.write('import pytest\n\nTODO')
--- progtool/cli/main.py
+++ progtool/cli/main.py
@@ -3,6 +3,7 @@ from progtool import log
 from progtool.cli.tree import tree
 from progtool.cli.server import server
 from progtool.cli.renumber import renumber
+from progtool.cli.create import create
 
 
 @click.group()
@@ -11,9 +12,26 @@ def cli(verbose):
     log.configure(verbose)
 
 
+@cli.command()
+def test():
+    from progtool import repository
+    from progtool.material.tree import create_material_tree
+    import asyncio
+
+    async def wait():
+        root.judge_recursively()
+        await asyncio.sleep(2)
+
+    root = create_material_tree(repository.find_exercises_root())
+    asyncio.run(wait())
+
+
 def process_command_line_arguments():
     commands = [
-        tree, server, renumber
+        tree,
+        server,
+        renumber,
+        create,
     ]
 
     for command in commands:
