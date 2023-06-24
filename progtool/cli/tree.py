82a2354ed5e4e4e3ea019bc715d5c5992e122aa9
Frederic Vogels <frederic.vogels@ucll.be>
2023-06-24 17:17:16 +0200

    tree command uses rich

--- progtool/cli/tree.py
+++ progtool/cli/tree.py
@@ -1,33 +1,34 @@
 from typing import cast
 from progtool.material.metadata import load_metadata
-from progtool.material.tree import Exercise, Section, Explanation, build_tree
+from progtool.material.tree import Exercise, MaterialTreeNode, Section, Explanation, build_tree
 from progtool.repository import find_exercises_root
 import click
-
-
-def _print_tree(help='Show tree'):
-    def recurse(node, indentation):
-        match node:
-            case Section():
-                section = cast(Section, node)
-                print(" " * indentation + section.name)
-                for child in section.children:
-                    recurse(child, indentation + 2)
-            case Exercise():
-                exercise = cast(Exercise, node)
-                print(" " * indentation + exercise.name)
-            case Explanation():
-                explanation = cast(Explanation, node)
-                print(" " * indentation + explanation.name)
-            case _:
-                assert False, 'Unrecognized node type'
-
-    root_path = find_exercises_root()
-    metadata = load_metadata(root_path)
-    tree = build_tree(metadata)
-    recurse(tree, 0)
+from rich.tree import Tree
+from rich.console import Console
 
 
 @click.command()
 def tree() -> None:
-    _print_tree()
+    def recurse(node: MaterialTreeNode, tree: Tree):
+        match node:
+            case Section():
+                section = cast(Section, node)
+                subtree = tree.add(section.name)
+                for child in section.children:
+                    recurse(child, subtree)
+            case Exercise():
+                exercise = cast(Exercise, node)
+                tree.add(f'[red]{exercise.name}[/red]')
+            case Explanation():
+                explanation = cast(Explanation, node)
+                tree.add(f'[blue]{explanation.name}[/blue]')
+            case _:
+                assert False, 'Unrecognized node type'
+
+    console = Console()
+    tree = Tree('root')
+    root_path = find_exercises_root()
+    metadata = load_metadata(root_path)
+    root = build_tree(metadata)
+    recurse(root, tree)
+    console.print(tree)
