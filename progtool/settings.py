73e0401006143dda15ef27cfb36dffac75b211cd
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-08-27 12:34:41 +0200

    settings functionality

--- progtool/settings.py
+++ progtool/settings.py
@@ -1,5 +1,41 @@
 import os
 from pathlib import Path
+from typing import Optional
+import yaml
+import pydantic
+
+
+class Settings(pydantic.BaseModel):
+    language_priorities: list[str]
+    html_path: Optional[str]
+    repository_root: Optional[str]
+    judgment_cache: str
+
+
+def settings_path() -> Path:
+    return Path.home() / 'progtool-settings.yaml'
+
+
+def create_settings_file() -> None:
+    with settings_path().open('w') as file:
+        yaml.dump(default_settings(), file)
+
+
+def default_settings():
+    return Settings(
+        language_priorities=['en', 'nl'],
+        html_path=None,
+        repository_root=None,
+        judgment_cache=default_judgment_cache_path()
+    )
+
+
+def load_settings():
+    path = settings_path()
+    if not path.exists():
+        create_settings_file()
+    with open(path) as file:
+        raw_settings = yaml.load(file)
 
 
 def language_priorities():
@@ -24,8 +60,12 @@ def repository_root() -> Path:
         return Path('G:/repos/ucll/programming/course-material')
 
 
-def judgment_cache() -> Path:
+def default_judgment_cache_path() -> Path:
     return Path.home() / "progtool-cache.json"
+
+
+def judgment_cache() -> Path:
+    return default_judgment_cache_path()
 
 
 def cache_delay() -> float:
